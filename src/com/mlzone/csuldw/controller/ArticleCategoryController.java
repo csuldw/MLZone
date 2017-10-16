2e7d00920f8ff832565001b3fc60157051b6b77b
dw <csu.ldw@csu.edu.cn>
2017-10-16 23:12:00 +0800

    add log4j to project

--- src/com/mlzone/csuldw/controller/ArticleCategoryController.java
+++ src/com/mlzone/csuldw/controller/ArticleCategoryController.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
