5171b70c058bce62c35805210d5b11afebf74cd1
Diwei Liu <csu.ldw@csu.edu.cn>
2017-12-25 17:14:13 +0800

    Create vo.ArticleCountStatVo.java

---
+++ src/com/mlzone/csuldw/entity/vo.ArticleCountStatVo.java
@@ -0,0 +1,27 @@
+package com.mlzone.csuldw.entity.vo;
+
+public class ArticleCountStatVo
+{
+	private int articleNumber;
+	private String queryType;
+
+	public int getArticleNumber()
+	{
+		return articleNumber;
+	}
+
+	public void setArticleNumber(int articleNumber)
+	{
+		this.articleNumber = articleNumber;
+	}
+
+	public String getQueryType()
+	{
+		return queryType;
+	}
+
+	public void setQueryType(String queryType)
+	{
+		this.queryType = queryType;
+	}
+}
