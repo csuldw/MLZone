7e1870ca49be818b158ffea29b3a27693420b719
Diwei Liu <csu.ldw@csu.edu.cn>
2018-01-09 16:40:27 +0800

    Update IArticleInfoMapper.java

--- src/com/mlzone/csuldw/dao/IArticleInfoMapper.java
+++ src/com/mlzone/csuldw/dao/IArticleInfoMapper.java
@@ -9,32 +9,6 @@ import com.mlzone.csuldw.entity.vo.ArticleCountStatVo;
 
 public interface IArticleInfoMapper extends BaseMapper<ArticleInfoEntity>{
 	
-    /**
-     * 根据条件查找文章，待完善
-     *
-     * Author:liudiwei
-     * Date:2017年10月14日
-     * @param keywords
-     * @param tag
-     * @param category
-     * @return
-     * @since
-     */
-    List<ArticleInfoEntity>getArticleInfoListByParams(String keywords, String tag, String category);
-
-    /**
-     * 根据条件统计文章数量，待完善
-     *
-     * Author:liudiwei
-     * Date:2017年10月14日
-     * @param keywords
-     * @param tag
-     * @param category
-     * @return
-     * @since
-     */
-    int countArticleInfoByParams(String keywords, String tag, String category);
-	
     List<ArticleCountStatVo> getArticleCountByQueryType(Object params);
     
     /**
